b0843fe57d41c638fde15408a2e7f97af91c7cd7
Anthony Anderson <LordIllyohs@gmail.com>
2014-09-03 15:36:43 -0400

    commiting to fix Dans branch
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/com/newlinegaming/Runix/helper/EnergyObjectHelper.java
+++ src/main/java/com/newlinegaming/Runix/helper/EnergyObjectHelper.java
@@ -2,10 +2,8 @@ package com.newlinegaming.Runix.helper;
 
 public class EnergyObjectHelper {
     
-    private int currentEnergy;
-    private int maximumEnrgey;
-    private int minimumEnrrgy;
-    
     //TODO: Find if class implements IItemEnergy or ITileEnergy
+    
+    
     
 }
